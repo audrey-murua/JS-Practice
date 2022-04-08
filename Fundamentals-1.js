a946aab1bb5253e2e02892a041ccbd2bae5883f3
audrey murua <audrey.murua1@gmail.com>
2022-04-07 23:14:34 -0500

    beginning fundamentals 1

---
+++ Fundamentals-1.js
@@ -0,0 +1,18 @@
+// Practice
+
+alert("Hello World!");
+
+let js = "amazing";
+
+// if (js === "amazing") {
+//     alert("JavaScript is awesome!");
+// }
+
+js = "boring";
+
+if (js === "amazing") {
+    alert("JavaScript is AWESOME!");
+} else {
+    alert("NO WAY!");
+}
+
