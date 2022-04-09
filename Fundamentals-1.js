c6dc3c462955ccef9f4ecbdc17cf5528b18682ee
audrey murua <audrey.murua1@gmail.com>
2022-04-08 23:16:54 -0500

    added notes and practice assignment

--- Fundamentals-1.js
+++ Fundamentals-1.js
@@ -1,6 +1,6 @@
 // Practice
 
-alert("Hello World!");
+console.log("Hello World!");
 
 let js = "amazing";
 
@@ -11,8 +11,48 @@ let js = "amazing";
 js = "boring";
 
 if (js === "amazing") {
-    alert("JavaScript is AWESOME!");
+    console.log("JavaScript is AWESOME!");
 } else {
-    alert("NO WAY!");
+    console.log("NO WAY!")
 }
 
+
+// Variables
+
+let firstName = "Audrey";
+console.log(firstName);
+console.log(firstName);
+
+//Variable names can have underscores
+
+let first_last_name = "Audrey Murua";
+console.log(first_last_name);
+
+//Descriptive variable names
+
+let oldestDog = "Hailey";
+let youngestDog = "Harley";
+let cleanestDog = "Navi";
+
+console.log(oldestDog);
+console.log(youngestDog);
+console.log(cleanestDog);
+
+//Constant variable names can be completely capitalized 
+const PI = 3.1415;
+
+
+
+//Practice Assignment - Values and Variables
+
+/* 1. Declare variables called 'country', 'continent' and 'population' and
+assign their values according to your own country (population in millions)
+2. Log their values to the console */
+
+let country = "United States";
+let continent = "North America";
+let population = "329.5 million";
+
+console.log(country);
+console.log(continent);
+console.log(population);
