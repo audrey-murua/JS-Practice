c6579eabea9cb478e346d9d6b77eb381393a7ff5
audrey murua <audrey.murua1@gmail.com>
2022-04-09 00:35:59 -0500

    completed data types assignment

--- Fundamentals-1.js
+++ Fundamentals-1.js
@@ -98,7 +98,17 @@ console.log(typeof null);
 
 //Practice Assignment - Data Types
 
-/* 1. Declareavariablecalled'isIsland'andsetitsvalueaccordingtoyour country. The variable should hold a Boolean value. Also declare a variable 'language', but don't assign it any value yet
-2. Logthetypesof'isIsland','population','country'and'language' to the console */
+/* 1. Declare a variable called'isIsland'and set its value according to your country. The variable should hold a Boolean value. Also declare a variable 'language', but don't assign it any value yet
+2. Log the typesof 'isIsland','population','country'and'language' to the console */
 
+let isIsland = false;
 
+let language;
+
+console.log(typeof isIsland);
+
+console.log(typeof language);
+
+console.log(typeof population);
+
+console.log(typeof country);
