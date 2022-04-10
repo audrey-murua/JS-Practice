85af6634e2eac701468c26246352ee817577a834
audrey murua <audrey.murua1@gmail.com>
2022-04-09 23:38:23 -0500

    completed let const var assignment

--- Fundamentals-1.js
+++ Fundamentals-1.js
@@ -49,8 +49,8 @@ const PI = 3.1415;
 assign their values according to your own country (population in millions)
 2. Log their values to the console */
 
-let country = "United States";
-let continent = "North America";
+// let country = "United States";
+// let continent = "North America";
 let population = "329.5 million";
 
 console.log(country);
@@ -101,7 +101,7 @@ console.log(typeof null);
 /* 1. Declare a variable called'isIsland'and set its value according to your country. The variable should hold a Boolean value. Also declare a variable 'language', but don't assign it any value yet
 2. Log the typesof 'isIsland','population','country'and'language' to the console */
 
-let isIsland = false;
+// let isIsland = false;
 
 let language;
 
@@ -112,3 +112,32 @@ console.log(typeof language);
 console.log(typeof population);
 
 console.log(typeof country);
+
+
+//DECLARING VARIABLES
+
+let age = 30;
+age = 31;
+
+const birthYear = 1950;
+
+var month = "march";
+month = "april";
+
+
+
+//Practice Assignment - Let, Const, Var
+
+/* 1. Set the valueof 'language' to the language spoken where you live(some countries have multiple languages, but just choose one)
+2. Think about which variables should be const variables(which values will never change, and which might change?). Then, change these variables to const.
+3. Try to change one of the changed variables now,and observe what happens */
+
+language = "english";
+
+const isIsland = false;
+
+const continent = "North America"
+
+const country = "United States";
+
+isIsland = true;
