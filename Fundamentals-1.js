7b8053328a5a2272a92707d156d33582bc15af46
audrey murua <audrey.murua1@gmail.com>
2022-04-18 23:57:41 -0500

    completed if/else statements assignment

--- Fundamentals-1.js
+++ Fundamentals-1.js
@@ -302,3 +302,27 @@ if(jonasBirthYear <= 2000) {
 }
 
 console.log(century);
+
+
+
+//Practice Assignment - If/Else Statements
+
+/* 1. If your country's population is greater than 33 million,log a string like this to the console: 'Portugal's population is above average'. Otherwise, log a string like 'Portugal's population is 22 million below average' (the 22 is the average of 33 minus the country's population)
+2. After checking the result, change the population temporarily to 13 and then to 130. See the different results, and set the population back to original */
+
+if(population > 33) {
+    console.log(`${country}'s population is above average.`);
+} else {
+    const belowAveragePopulation = 33 - population;
+    console.log(`${country}'s population is ${belowAveragePopulation} million below average.`);
+}
+
+// population = 13;
+// population = 130;
+
+// if(population > 33) {
+//     console.log(`${country}'s population is above average.`);
+// } else {
+//     const belowAveragePopulation = 33 - population;
+//     console.log(`${country}'s population is ${belowAveragePopulation} million below average.`);
+// }
